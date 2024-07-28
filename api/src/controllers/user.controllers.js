24dce925f8dbdb9e8fae6f57f4df8024285c4e7b
dino2580 <anshgoel2580@gmail.com>
2024-07-28 05:53:42 +0530

    backend

---
+++ api/src/controllers/user.controllers.js
@@ -0,0 +1,70 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.models.js";
+
+export const registerUser = asyncHandler(async (req,res) => {
+    // console.log(req.body)
+    try {
+    const {
+        name,
+        religion,
+        income,
+        gender,
+        caste,
+        maritalStatus,
+        age,
+        houseOwned,
+        phoneNumber,
+        aadharNumber,
+        region,
+        education
+
+    } = req.body
+
+    const exisitingUser = await User.findOne(
+        {
+            name,
+            religion,
+            income,
+            gender,
+            caste,
+            maritalStatus,
+            age,
+            houseOwned,
+            phoneNumber,
+            aadharNumber,
+            region,
+            education
+        }
+    )
+    // console.log(exisitingUser);
+    if(exisitingUser) {
+        throw new ApiError(404,"user already exists")
+    }
+
+    const newUser = await User.create(
+        {
+            name,
+            religion,
+            income,
+            gender,
+            caste,
+            maritalStatus,
+            age,
+            houseOwned,
+            phoneNumber,
+            aadharNumber,
+            region,
+            education
+        }
+    )
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,newUser,"User Registered Sucessfully"))
+}
+     catch (error) {
+        throw new ApiError(404,"Registration Failed",error.message)
+    }
+})
